faf9ed6b8db34801c6470d74c2e042754b9c76f9
u6943702 <u6943702@anu.edu.au>
2021-02-26 14:24:52 +1100

    Add MoneyWise.js

---
+++ client/src/MoneyWise.js
@@ -0,0 +1,43 @@
+import React from "react";
+import SectionHeader from "./SectionHeader";
+import Navbar from "./Navbar";
+import Catalog from "./Catalog";
+import Footer from "./Footer";
+
+class MoneyWise extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            hamburgerBtnState: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    } 
+
+    handleClick(evt) {
+        evt.preventDefault();
+        if (this.state.hamburgerBtnState) {
+            this.setState({
+                hamburgerBtnState: false
+            });
+            evt.currentTarget.classList.remove("open");
+        } else {
+            this.setState({
+                hamburgerBtnState: true
+            });
+            evt.currentTarget.classList.add("open");
+        }
+    }
+    
+    render() {
+        return (
+            <section className="moneywise-wrapper section">
+                <SectionHeader function={this.handleClick}/>
+                <Navbar burgerState={this.state}/>
+                <Catalog section="Moneywise"/>
+                <Footer />
+            </section>
+        );
+    }
+}
+
+export default MoneyWise;
