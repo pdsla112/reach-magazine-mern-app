4f2fee3ac57b852e17b1c8d5fca0045ae6b92e32
u6943702 <u6943702@anu.edu.au>
2021-02-26 14:31:48 +1100

    Add SubscriptionAd.js

---
+++ client/src/SubscriptionAd.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class SubscriptionAd extends React.Component {
+    render() {
+        return(
+            <div className="subscription-ad-wrapper">
+                    <p className="subscription-ad-words">GET YOUR VOICE HEARD. GET 2 MONTHS FOR ONLY $8</p>
+                    <button className="subscription-ad-btn">SUBSCRIBE NOW</button>
+            </div>
+        );
+    }
+}
+
+export default SubscriptionAd;
