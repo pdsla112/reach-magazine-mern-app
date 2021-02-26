fddc94dd9e63d7b8acc779ab12acd2b8fc45f154
u6943702 <u6943702@anu.edu.au>
2021-02-26 14:23:34 +1100

    Add Medicine.js and MedicineHome.js

---
+++ client/src/MedicineHome.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { withRouter } from "react-router-dom";
+import HomeSection from "./HomeSection";
+
+class MedicineHome extends React.Component {
+    _isMounted = false;
+    constructor(props) {
+        super(props);
+    }
+
+    async componentDidMount() {
+        this._isMounted = true;
+    }
+
+    componentWillUnmount() {
+        this._isMounted = false;
+    }
+
+    render() {
+        return(
+            <HomeSection section="medicine"/>
+        );
+    }
+}
+
+export default withRouter(MedicineHome);
