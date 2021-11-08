63f0125e4d863c714f90daf3b35a543afdd49e2d
pdsla112@hotmail.com <pdsla112@hotmail.com>
2021-11-08 22:17:45 +1100

    Test

--- client/src/Footer.js
+++ client/src/Footer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import NavLink from "react-router-dom";
+import {NavLink} from "react-router-dom";
 import styled from "styled-components";
 import facebookLogo from "./assets/facebook-logo.svg";
 import instagramLogo from "./assets/instagram-logo.svg";
