77edc44d7c21eed80ae838e537fe933b28aacbac
pdsla112@hotmail.com <pdsla112@hotmail.com>
2021-11-08 21:45:42 +1100

    Test

--- index-helper.js
+++ index-helper.js
@@ -2,7 +2,10 @@ require('ignore-styles')
 
 require('@babel/register')({
     ignore: [/(node_module)/],
-    presets: ['@babel/preset-env', '@babel/preset-react']
+    presets: ['@babel/preset-env', '@babel/preset-react'],
+    plugins: [
+        "@babel/plugin-transform-runtime"
+      ]
 })
 
 require('./index')
