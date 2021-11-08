ba62714408de282894d9aa220c95a035f646a78e
pdsla112@hotmail.com <pdsla112@hotmail.com>
2021-11-08 15:02:19 +1100

    Update to renderToString

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {BrowserRouter as Router} from "react-router-dom";
 
-ReactDOM.render(
+ReactDOM.renderToString(
   <Router>
     <React.StrictMode>
       <App/>
